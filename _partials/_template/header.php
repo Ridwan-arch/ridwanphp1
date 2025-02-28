725ad9848f3c97373e043c140e9d938f19f96ac0
Ridwan-arch <ridwan867k@gmail.com>
2025-02-28 10:59:19 +0700

    Project saya hari ini

---
+++ _partials/_template/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg bg-success border-bottom border-5 border-warning">
+    <div class="container-fluid">
+    
+      <a class="navbar-brand text-white fw-bold" href="#">
+        <h1 class="mb-0"><i class="bi bi-boxes text-warning"></i><b>&nbsp; RidwanAI</b></h1>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+        aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navbarScroll">
+        <ul class="navbar-nav me-3 my-2 my-lg-0 navbar-nav-scroll">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#" style="font-size: 20px;">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" style="font-size: 20px;">Subscription</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" style="font-size: 20px;">AI Services</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false" style="font-size: 20px;">
+              Help Center
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">API Integrations</a></li>
+              <li><a class="dropdown-item" href="#">Embedded AI Chatbots</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Cloud Datasets</a></li>
+            </ul>
+          </li>
+        </ul>
+
+         <!-- Sign Button -->
+         <a href="#" class="btn btn-warning fw-bolder" style="border-radius: 20px;">Sign Up <i class="bi bi-brush-fill "></i></a>
+        </div>
+      </div>
+    </nav>
---
+++ file_/home.php
@@ -0,0 +1,85 @@
+<?php include '.\_partials\_template\header.php';?>
+
+    <div class="container mt-4">
+      <div class="p-5 text-dark  d-flex align-items-center position-relative" style="border-top: 4px solid gold; 
+                    border-right: 20px solid gold; 
+                    border-top-right-radius: 40px;
+                    border-radius: 70%/20%;">
+
+
+<!-- Image on the left -->
+<!-- <img src="image/robot-ai.png" alt="AI Robot" class="img-fluid"
+style="width: 200px; height: auto; position: absolute; bottom: 10px; left: 20px;"> -->
+
+<!-- Text Content -->
+<div class="" >
+  <h1 style="font-size:60px;"><b>RidwanAI The Answer You Want</b></h1>
+  <p class="text-muted">Gunakan Futuristik AI chatbot di lokal server kamu<br />Layanan Chat bot virtual yang
+    bisa kamu gunain untuk kegiatan sehari-hari yang terintegrasi ke aplikasi kamu </p>
+    
+  </div>
+</div>
+</div>  
+
+<!-- Features Section -->
+<div class="container mt-5">
+  <h2 class="mb-3 fw-bolder">RidwanAI Convenient And Smart</h2>
+  <p>Due to high request of AI, The next generation AI Chatbot has been created to be reusable in code optimization in Aurora Server</p>
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">     
+    
+    <!-- Feature 1 -->
+    <div class="col">
+      <div class="card p-3  bg-light" 
+      style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius:  70%/20%;">
+        <i class="bi bi-airplane-engines text-primary fs-1"></i>
+        <h5 class="mt-3"><b>Smart Airplane</b></h5>
+        <p>RidwanAI provides the airplane service you want, with cheap prices you can go anywhere buddy...</p>
+      </div>
+    </div>
+    
+    <!-- Feature 2 -->
+    <div class="col">
+      <div class="card p-3 bg-light " 
+      style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 70%/20%;">
+        <i class="bi bi-bus-front text-danger fs-1"></i>
+        <h5 class="mt-3"><b>Electric Bus</b></h5>
+        <p>RidwanAI provides fast electric bus services, you only need a short wait to reach your destination</p>
+      </div>
+    </div>
+
+    <!-- Feature 3 -->
+    <div class="col">
+      <div class="card p-3 bg-light " 
+           style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 70%/20%;">
+        <i class="bi bi-archive text-success fs-1"></i>
+        <h5 class="mt-3"><b>Archive</b></h5>
+        <p>RidwanAI always strictly guards your privacy, never doubt ChatZone regarding your privacy</p>
+      </div>
+    </div>
+
+    <!-- Feature 4 -->
+    <div class="col">
+      <div class="card p-3 bg-light " 
+           style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 70%/20%;">
+        <i class="bi bi-alarm text-warning fs-1"></i>
+        <h5 class="mt-3"><b>Golden Time</b></h5>
+        <p>RidwanAI is always on time for all you orders, you can set the time you want if you want</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+  </div>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>     
